03c46083d558c6c4baf6ab30dd3b2ba53ea4a8c2
aliyun-k <22542812@qq.com>
2018-04-27 00:29:37 +0800

    design

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<a href="/yaoyao/index.php">yoyo</a><br>
-<a href="/yu/index.php">yu</a><br>
-<a href="/k/index.php">k</a><br>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+<style>
+body{
+    text-align: center;
+}
+#title{ 
+}
+a{
+   color:rgb(6, 86, 222);
+   text-decoration:none;
+}
 
+</style>
+<title>效果图</title>
+</head> 
+<body >
+<h3>K-store</h3><br>
+<a href = "./design/design.php">效果图</a><br>
+<a href = "./k/home/index.php">Welcome</a><br>
+<a href = "./k/admin/admin.php">Admin</a><br>
 
+</body>
+</html>
--- k/App/config.php
+++ k/App/config.php
@@ -1,6 +1,6 @@
 <?php 
 $user = "k";
-$root = "/home/ki/nginx/www/".$user."/";
+$root = "/home/k/www/".$user."/";
 //DB 
 $db_type='mysql';
 $db_host='127.0.0.1';
